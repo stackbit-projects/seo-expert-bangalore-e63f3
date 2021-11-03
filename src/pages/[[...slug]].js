d37919e134d94066f387602ba54ce1d6fa0af7df
Stackbit Code Editor <projects@stackbit.com>
2021-11-03 12:48:51 +0000

    [[...slug]].js: changed

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -16,7 +16,7 @@ class Page extends React.Component {
     }
 }
 
-export defer function getStaticPaths() {
+export asyn function getStaticPaths() {
     console.log('Page [...slug].js getStaticPaths');
     const paths = await sourcebitDataClient.getStaticPaths();
     return { paths, fallback: false };
